b6868e7df6f142b2ecdaca3c5f61567b8c0259c2
gm2870 <alifarzanegan70@gmail.com>
2019-08-05 01:37:30 +0430

    search controller is done --video 14

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,30 @@
 // Global app controller
+// https://www.food2fork.com/api/search
+// 1b71bac192094f76fa22af4a95983e6f
+import Search from './models/Search';
+/** Global state of the app
+ *  search object
+ *  current recipe object
+ *  shopping list object
+ *  liked recepies
+ */
+const state = {};
+const controlSearch = async () => {
+    // 1) get query from the view
+    const query = 'pizza';
+    if(query){
+        // 2) new search object and add to state
+        state.search = new Search(query);
+        // 3) prepare UI for results
 
+        // 4) search for recepies
+        await state.search.getResults();
+        // 5) render results to UI
+    }
+}
+
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
